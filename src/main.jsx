b4c21e4100b0b4b0a7e3e59d70c5a003548eb286
itsLohit <109834876+itsLohit@users.noreply.github.com>
2025-09-22 15:33:03 +0530

    fix: replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import routes from "./routes";
 import './index.css';
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
